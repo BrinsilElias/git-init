3f0f134ad36863d7dd1e89381d21fb37be94af7f
Brinsil Elias <brinsilelias0592@gmail.com>
2024-11-14 12:57:59 +0530

    feat: add git-init cli prompts
    Changes made:
    1 - Migrate to @clack/prompts for cli prompts
    2 - Migrate to biome for formating and liniting
    3 - Removed vitest for unit testing
    4 - Create cli prompts

--- src/core/options.ts
+++ src/core/options.ts
@@ -1,72 +1,107 @@
-import type { PromptObject } from "prompts"
 import pc from "picocolors"
+import * as constants from "./constants"
+import { text, select, group, log } from "@clack/prompts"
+import { makeApiRequest, _cancel as cancel } from "./utils"
 
-const projectDirectory: PromptObject = {
-  type: "select",
-  name: "projectDirectory",
-  message: pc.bold("Initialize git in:"),
-  choices: [
-    { title: "Current directory", value: "currentDir" },
-    { title: "Specify directory", value: "newDir" },
-  ],
+async function initGitPrompts() {
+  return group(
+    {
+      projectDirectory: () =>
+        select({
+          message: pc.bold("Initialize git in:"),
+          initialValue: "currentDir",
+          options: [
+            { value: "currentDir", label: "Current directory" },
+            { value: "newDir", label: "Specify directory" },
+          ],
+        }),
+
+      specifyDirectoryPath: ({ results }) => {
+        if (results.projectDirectory === "newDir") {
+          return text({
+            message: pc.bold("Specify the directory path"),
+            placeholder: "./new-project",
+            validate: (value) => {
+              if (!value) return "Please enter a path."
+              if (value[0] !== ".") return "Please enter a relative path."
+            },
+          })
+        }
+        return
+      },
+
+      chooseLicense: async () => {
+        const licenseChoices = await makeApiRequest(constants.LISCENSES_API_URL, "GET").catch(
+          () => {
+            log.error(pc.bold(pc.redBright("Failed to fetch licenses")))
+            return
+          },
+        )
+        if (!licenseChoices) {
+          return
+        }
+        const choices = licenseChoices.map(
+          (item: {
+            key: string
+            name: string
+            spdx_id: string
+            url: string
+            node_id: string
+          }) => ({
+            title: item.name,
+            value: item.key,
+          }),
+        )
+        return select({
+          message: pc.bold("Choose a license"),
+          options: [{ value: false, label: "None" }, ...choices],
+          maxItems: 10,
+        })
+      },
+
+      chooseGitignore: async () => {
+        const gitIgnoreChoices = await makeApiRequest(constants.GITIGNORE_API_URL, "GET").catch(
+          () => {
+            log.error(pc.bold(pc.redBright("Failed to fetch gitignore template")))
+            return
+          },
+        )
+        if (!gitIgnoreChoices) {
+          return
+        }
+        const choices = gitIgnoreChoices.map((item: string) => ({
+          title: item,
+          value: item,
+        }))
+        return select({
+          message: pc.bold("Choose a .gitignore template"),
+          options: [{ value: false, label: "None" }, ...choices],
+          maxItems: 10,
+        })
+      },
+
+      mainBranchName: () =>
+        text({
+          message: pc.bold("Enter the name of the main branch"),
+          defaultValue: "main",
+          placeholder: "main",
+        }),
+
+      remoteRepoUrl: () =>
+        text({
+          message: pc.bold("Enter the remote repository URL"),
+          placeholder: "https://www.github.com/username/repo",
+          validate: (value) => {
+            if (!value.includes("https")) return "Please enter a valid URL."
+          },
+        }),
+    },
+    {
+      onCancel: () => {
+        cancel("  Operation cancelled  ")
+      },
+    },
+  )
 }
 
-const speccifyDirectoryPath: PromptObject = {
-  type: (prev) => (prev === "newDir" ? "text" : null),
-  name: "specifyDirectoryPath",
-  message: pc.bold("Specify the directory path"),
-}
-
-const authorNamePrompt: PromptObject = {
-  type: "text",
-  name: "authorName",
-  message: pc.bold("What is the author name?"),
-}
-
-const addGitIgnorePrompt: PromptObject = {
-  type: "confirm",
-  name: "shouldAddGitIgnore",
-  message: pc.bold("Do you want to add a .gitignore file?"),
-  initial: true,
-}
-
-const licensePrompt: PromptObject = {
-  type: "select",
-  name: "chosenLicense",
-  message: pc.bold("Choose a license"),
-  choices: [
-    { title: "MIT", value: "MIT" },
-    { title: "Apache 2.0", value: "Apache 2.0" },
-    { title: "GPL 3.0", value: "GPL 3.0" },
-  ],
-}
-
-const mainBranchPrompt: PromptObject = {
-  type: "text",
-  name: "mainBranchName",
-  message: pc.bold("Enter the name of the main branch"),
-}
-
-const addRemoteRepoPrompt: PromptObject = {
-  type: "confirm",
-  name: "shouldAddRemoteRepo",
-  message: pc.bold("Do you want to add a remote repository?"),
-  initial: true,
-}
-
-const remoteRepoUrlPrompt: PromptObject = {
-  type: "text",
-  name: "remoteRepoUrl", // This one is okay too!
-  message: pc.bold("Enter the remote repository URL"),
-}
-
-export const promptArray: Array<PromptObject> = [
-  projectDirectory,
-  speccifyDirectoryPath,
-  authorNamePrompt,
-  addGitIgnorePrompt,
-  licensePrompt,
-  mainBranchPrompt,
-  addRemoteRepoPrompt,
-  remoteRepoUrlPrompt,
-]
+export default initGitPrompts
--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,83 +1,122 @@
 import * as fs from "node:fs"
-import * as path from "node:path"
-import { exec } from "node:child_process"
+import pc from "picocolors"
+import { promisify } from "node:util"
+import { log, cancel } from "@clack/prompts"
+import { execSync } from "node:child_process"
 
-export async function createFolder(folderPath: string): Promise<void> {
-  const absolutePath = path.resolve(folderPath)
+const fsWriteFile = promisify(fs.writeFile)
+const fsExists = promisify(fs.exists)
+const fsMkdir = promisify(fs.mkdir)
 
-  // Use a promise to handle asynchronous behavior without external dependencies
-  return new Promise((resolve, reject) => {
-    // Check if folder exists
-    fs.access(absolutePath, fs.constants.F_OK, (err) => {
-      if (!err) {
-        console.log(`Folder "${absolutePath}" already exists.`)
-        resolve()
-      } else if (err.code === "ENOENT") {
-        // Folder does not exist, create it
-        fs.mkdir(absolutePath, { recursive: true }, (mkdirErr) => {
-          if (mkdirErr) {
-            console.error(
-              `Error creating folder "${absolutePath}": ${mkdirErr.message}`,
-            )
-            reject(mkdirErr)
-          } else {
-            console.log(`Folder "${absolutePath}" created successfully.`)
-            resolve()
-          }
-        })
-      } else {
-        // Some other error while accessing the folder
-        console.error(
-          `Error accessing folder "${absolutePath}": ${err.message}`,
-        )
-        reject(err)
-      }
-    })
-  })
+interface File {
+  name: string
+  content: string
+  path: string
 }
 
-/**
- * Utility function to initialize a Git repository in the specified folder.
- *
- * @param repoPath - The path where the Git repository should be initialized.
- * @returns Promise<void> - Resolves if the Git repository is initialized successfully, rejects if there is an error.
- */
-export async function initializeGit(repoPath: string): Promise<void> {
-  const absolutePath = path.resolve(repoPath)
+export const createDirectory = async (pathDir: string): Promise<void> => {
+  try {
+    const exists = await fsExists(pathDir)
+    if (exists) {
+      log.warn(pc.bold(pc.yellowBright("Directory already exists.")))
+      log.message()
+      return Promise.resolve()
+    }
+    await fsMkdir(pathDir)
+    return Promise.resolve()
+  } catch (_) {
+    Promise.reject()
+  }
+  return Promise.resolve()
+}
 
-  return new Promise((resolve, reject) => {
-    // Use child_process to execute the git init command
-    exec("git init", { cwd: absolutePath }, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error initializing Git repository: ${stderr}`)
-        reject(error)
-      } else {
-        console.log(`Git repository initialized at ${absolutePath}`)
-        resolve()
-      }
-    })
-  })
+export const createFiles = async (file: File): Promise<boolean> => {
+  try {
+    const filePath = `${file.path}/${file.name}`
+    await fsWriteFile(filePath, file.content.toString())
+  } catch (_) {
+    return false
+  }
+  return true
 }
 
 export async function makeApiRequest(
   url: string,
-  method: "GET" | "POST" | "PUT" | "DELETE" = "GET",
+  method: "GET" | "POST" | "PUT" | "DELETE",
+  query?: Record<string, string>,
 ) {
-  const options: RequestInit = {
-    method,
+  let apiEndPoint = url
+  const options = { method }
+
+  if (query) {
+    const queryString = new URLSearchParams(query).toString()
+    apiEndPoint += `?${queryString}`
   }
 
   try {
-    const response = await fetch(url, options)
-
+    const response = await fetch(apiEndPoint, options)
     if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`)
+      return Promise.reject()
     }
-
     const data = await response.json()
     return data
-  } catch (error) {
-    console.error(`Error fetching ${url}:`, error)
-    throw error
+  } catch (_) {
+    return Promise.reject()
   }
+}
+
+export function initializeGit(path: string | unknown): { error: boolean; message: string } {
+  if (isGitInitialized({ cwd: path, stdio: "ignore" })) {
+    return { error: true, message: "Git is already initialized." }
+  }
+  try {
+    execSync(`git init ${path}`)
+  } catch (_) {
+    return { error: true, message: "Failed to initialize git." }
+  }
+  return { error: false, message: "Git initialized successfully." }
+}
+
+export function isGitInitialized(options?: Record<string, unknown>): boolean {
+  try {
+    execSync("git rev-parse --is-inside-work-tree", options)
+  } catch (_) {
+    return false
+  }
+  return true
+}
+
+export function setBranchName(
+  branchName: string,
+  options?: Record<string, unknown>,
+): { error: boolean; message: string } {
+  if (!isGitInitialized(options)) {
+    return { error: true, message: "Git is not initialized" }
+  }
+  try {
+    execSync(`git branch -m ${branchName}`, options)
+  } catch (_) {
+    return { error: true, message: "Failed to set branch name." }
+  }
+  return { error: false, message: "Branch name set successfully." }
+}
+
+export function setRemoteUrl(
+  url: string,
+  options?: Record<string, unknown>,
+): { error: boolean; message: string } {
+  if (!isGitInitialized(options)) {
+    return { error: true, message: "Git is not initialized" }
+  }
+  try {
+    execSync(`git remote add origin ${url}`, options)
+  } catch (_) {
+    return { error: true, message: "Failed to set remote URL." }
+  }
+  return { error: false, message: "Remote URL set successfully." }
+}
+
+export function _cancel(message: string): void {
+  cancel(pc.bgRedBright(message))
+  process.exit(1)
 }
